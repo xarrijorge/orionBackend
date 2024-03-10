931612a7c44bb7e449d15240c0bbe47cb16a44c1
perismuthoni <muthonimuriuki22@gmail.com>
2024-03-10 22:37:50 +0000

    entry file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from Flask-SQLAlchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app.models import *
 
 # Import routes after creating the Flask app to avoid circular imports
 from app.routes import *
