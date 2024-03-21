79f1dc7690eef381f20868ff8bf623f3c031c157
perismuthoni <muthonimuriuki22@gmail.com>
2024-03-21 16:04:16 +0000

    updated configurations

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 
 
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+AUTHORIZATION_URL = os.environ.get("auth_uri", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+SECRET_KEY = os.environ.get("secret_key", None)
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
